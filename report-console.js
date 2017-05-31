606b775299c7bf62bc9107c23d844c7f8b5069d2
sintell <a.anatskiy@hh.ru>
2017-05-31 19:46:20 +0300

    HH-69318 use new format of result reporting

--- report-console.js
+++ report-console.js
@@ -3,7 +3,7 @@
 var gutil = require('gulp-util');
 var chalk = require('chalk');
 
-module.exports = function(error, result, file) {
+module.exports = function(error, result) {
     var passed = true;
     var out;
     var stats;
@@ -11,31 +11,29 @@ module.exports = function(error, result, file) {
 
     if (result) {
         try {
-            result.trim().split('\n').forEach(function(line) {
-                if (line.indexOf('{') !== -1) {
-                    out = JSON.parse(line.trim());
+            if (result.indexOf('{') !== -1) {
+                out = JSON.parse(result);
 
-                    stats = out.stats;
+                stats = out.data.stats;
 
-                    color = stats.failures > 0 ? chalk.red : chalk.green;
+                color = stats.failures > 0 ? chalk.red : chalk.green;
 
-                    gutil.log('Took ' + stats.duration + ' ms to run ' + chalk.blue(stats.tests) + ' tests. ' + color(stats.passes + ' passed, ' + stats.failures + ' failed.'));
+                gutil.log('Took ' + stats.duration + ' ms to run ' + chalk.blue(stats.tests) + ' tests. ' + color(stats.passes + ' passed, ' + stats.failures + ' failed.'));
 
-                    if (out.failures) {
-                        out.failures.forEach(function(item) {
-                            gutil.log(chalk.red('Test failed') + ': ' + chalk.red(item.fullTitle) + ': \n' + item.error);
-                        });
-                    }
+                if (stats.failures > 0) {
+                    gutil.log('gulp-qunit: ' + chalk.red('✖ ') + 'QUnit assertions failed in ' + chalk.blue(out.file));
+                } else {
+                    gutil.log('gulp-qunit: ' + chalk.green('✔ ') + 'QUnit assertions all passed in ' + chalk.blue(out.file));
                 }
-            });
+
+                if (out.data.failures) {
+                    out.data.failures.forEach(function(item) {
+                        gutil.log(chalk.red('Test failed') + ': ' + chalk.red(item.fullTitle) + ': \n' + item.error);
+                    });
+                }
+            }
         } catch (e) {
             gutil.log(chalk.red(e));
         }
-    }
-
-    if (error) {
-        gutil.log('gulp-qunit: ' + chalk.red('✖ ') + 'QUnit assertions failed in ' + chalk.blue(file.relative));
-    } else {
-        gutil.log('gulp-qunit: ' + chalk.green('✔ ') + 'QUnit assertions all passed in ' + chalk.blue(file.relative));
     }
 };
